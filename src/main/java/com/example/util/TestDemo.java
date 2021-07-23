23676d55e5ccd50dd7f6da3df28b86552fa42ddd
hututu <1093958707@qq.com>
2021-07-23 13:28:53 +0800

    更新订单模块 添加异步回调

--- src/main/java/com/example/util/TestDemo.java
+++ src/main/java/com/example/util/TestDemo.java
@@ -13,17 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.*;
 
 @RestController
 
-public class TestDemo
-{
+public class TestDemo {
 
     @RequestMapping(value = "xxx")
     //获取openid
-    public static Map<String, Object> getWxUserOpenid(String code)
-    {
+    public static Map<String, Object> getWxUserOpenid(String code) {
         //拼接url
         StringBuilder url = new StringBuilder("https://api.weixin.qq.com/sns/jscode2session?");
         url.append("appid=wx14761dfae2eaae64");//appid设置
@@ -34,8 +33,7 @@ public class TestDemo
         url.append("&connect_redirect=1");
         System.out.println(url);
         Map<String, Object> map = null;
-        try
-        {
+        try {
             HttpClient client = HttpClientBuilder.create().build();//构建一个Client
             HttpGet get = new HttpGet(url.toString());    //构建一个GET请求
             HttpResponse response = client.execute(get);//提交GET请求
@@ -46,36 +44,29 @@ public class TestDemo
             //把信息封装到map
             map = parseJSON2Map(res);//这个小工具的代码在下面
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
         return map;
     }
 
     //json转map,这个小工具是我从网上找的,谢谢作者
-    public static Map<String, Object> parseJSON2Map(JSONObject json)
-    {
+    public static Map<String, Object> parseJSON2Map(JSONObject json) {
         Map<String, Object> map = new HashMap<String, Object>();
         // 最外层解析
-        for (Object k : json.keySet())
-        {
+        for (Object k : json.keySet()) {
             Object v = json.get(k);
             // 如果内层还是数组的话，继续解析
-            if (v instanceof JSONArray)
-            {
+            if (v instanceof JSONArray) {
                 List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
                 @SuppressWarnings("unchecked")
                 Iterator<JSONObject> it = ((JSONArray) v).iterator();
-                while (it.hasNext())
-                {
+                while (it.hasNext()) {
                     JSONObject json2 = it.next();
                     list.add(parseJSON2Map(json2));
                 }
                 map.put(k.toString(), list);
-            }
-            else
-            {
+            } else {
                 map.put(k.toString(), v);
             }
         }
